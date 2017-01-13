2bc7b61f06901361b2086e447c0b5e87c86ac85e
Aidin777 <arnold1990@yandex.ru>
2017-01-13 18:07:44 +0300

    Тестовый Файл1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+	require ('bd_model.php');
+?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Список статей</title>
+</head>
+<body>
+<?php
+select_all();
+?>
+
+<a href="inde.php">Список статей</a>
+<a href="add.php">Добавить статью</a>
+<a href="delete.php">Удалить статью</a>
+<a href="article.php">Одна статья</a>
+<a href="edit.php">Редактировать статью</a>
+</body>
+</html>
